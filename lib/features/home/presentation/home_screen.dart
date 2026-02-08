357682dc461995f49a9aec9abf52a7d7c192fb1b
Shivansh722 <shivanshcha722@gmail.com>
2026-02-08 16:27:20 +0530

    feat: anim 3

--- lib/features/home/presentation/home_screen.dart
+++ lib/features/home/presentation/home_screen.dart
@@ -605,7 +605,7 @@ class _HomeScreenState extends State<HomeScreen> with TickerProviderStateMixin {
                             child: Row(
                               children: [
                                 Text(
-                                  '📆Today, ',
+                                  'Today, ',
                                   style: TextStyle(
                                     fontSize: 24.sp,
                                     fontWeight: FontWeight.w600,
